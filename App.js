aefb8dc4f737b3155d70ebcd00c15bbe5741c5e5
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-01-26 21:44:38 +0000

    add initial auth, add firebase setup

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import { auth } from "./firebase";
+import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, setUser);
+  }, []);
+
+  useEffect(() => {
+    console.info({ user });
+  }, [user]);
+
+  const signIn = async () => {
+    await signInAnonymously(auth);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={signIn} title="Sign In"></Button>
+      <Text>{user ? "Logged in" : "Logged out"}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+
+import { firebaseConfig } from "./firebaseConfig";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.info("Initialised App");
+
+const db = getFirestore(app);
+// connectFirestoreEmulator(db, "http://localhost", 8080);
+connectFirestoreEmulator(db, "http://10.0.2.2", 8080);
+
+console.info("Initialised DB");
+
+const storage = getStorage(app);
+// connectStorageEmulator(storage, "http://localhost", 9199);
+connectStorageEmulator(storage, "http://10.0.2.2", 9199);
+
+console.info("Initialised Storage");
+
+const functions = getFunctions(app);
+// connectFunctionsEmulator(functions, "http://localhost", 5001);
+connectFunctionsEmulator(functions, "http://10.0.2.2", 5001);
+
+console.info("Initialised Functions");
+
+const auth = getAuth(app);
+// connectAuthEmulator(auth, "http://localhost:9099");
+connectAuthEmulator(auth, "http://10.0.2.2:9099");
+
+console.info("Initialised Auth");
+
+export { db, auth, storage, functions };
