0782d9d67b63dc1c53044e240ce99ea67a5ca65d
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-01-29 14:27:57 +0000

    initial firestore and storage interactions

--- App.js
+++ App.js
@@ -1,30 +1,111 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 
-import { auth } from "./firebase";
-import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { auth, db, storage } from "./firebase";
+import { signInAnonymously, onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  where,
+} from "firebase/firestore";
+import {
+  uploadString,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 import { useEffect, useState } from "react";
 
 export default function App() {
   const [user, setUser] = useState(null);
+  const [records, setRecords] = useState(0);
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    onAuthStateChanged(auth, setUser);
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   useEffect(() => {
     console.info({ user });
   }, [user]);
 
+  useEffect(() => {
+    if (!user) return;
+
+    const collectionQuery = query(
+      collection(db, "users"),
+      where("userId", "==", user.uid)
+    );
+    const unsubscribe = onSnapshot(collectionQuery, (snapshot) => {
+      console.log("Current data: ", snapshot, snapshot.size);
+      setRecords(snapshot.size);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [user]);
+
   const signIn = async () => {
     await signInAnonymously(auth);
   };
 
+  const handleSignOut = async () => {
+    await signOut(auth);
+  };
+
+  const addDocument = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        userId: user.uid,
+        created: serverTimestamp(),
+        updated: serverTimestamp(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const addDataUrlFile = async () => {
+    console.info("uploading");
+
+    const storageRef = ref(storage, "file.txt");
+
+    const message = "'This is my message.'";
+
+    const result = await uploadString(storageRef, message);
+    console.info("uploaded", { result });
+
+    const newUrl = await getDownloadURL(storageRef);
+    setUrl(newUrl);
+  };
+
   return (
     <View style={styles.container}>
-      <Button onPress={signIn} title="Sign In"></Button>
-      <Text>{user ? "Logged in" : "Logged out"}</Text>
+      {user ? (
+        <Button onPress={handleSignOut} title="Sign Out" />
+      ) : (
+        <Button onPress={signIn} title="Sign In" />
+      )}
+      {user ? (
+        <View>
+          <View>
+            <Button title="Add firestore record" onPress={addDocument} />
+            <Text>Current records: {records}</Text>
+          </View>
+          <View>
+            <Button title="Add storage data URL" onPress={addDataUrlFile} />
+            <Text>Download URL: {url}</Text>
+          </View>
+        </View>
+      ) : null}
       <StatusBar style="auto" />
     </View>
   );
@@ -35,6 +116,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+    paddingTop: 50,
   },
 });
--- firebase.js
+++ firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+  initializeFirestore,
+} from "firebase/firestore";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
@@ -18,24 +22,33 @@ console.info("Initialised App");
 
 const db = getFirestore(app);
 // connectFirestoreEmulator(db, "http://localhost", 8080);
-connectFirestoreEmulator(db, "http://10.0.2.2", 8080);
+// connectFirestoreEmulator(db, "http://127.0.0.1", 8080);
+// connectFirestoreEmulator(db, "http://192.168.0.14", 8080);
+connectFirestoreEmulator(db, "10.0.2.2", 8080);
+// connectFirestoreEmulator(db, "http://10.0.2.2", 8080);
 
 console.info("Initialised DB");
 
 const storage = getStorage(app);
 // connectStorageEmulator(storage, "http://localhost", 9199);
-connectStorageEmulator(storage, "http://10.0.2.2", 9199);
+// connectStorageEmulator(storage, "http://127.0.0.1", 9199);
+// connectStorageEmulator(storage, "http://192.168.0.14", 9199);
+connectStorageEmulator(storage, "10.0.2.2", 9199);
 
 console.info("Initialised Storage");
 
 const functions = getFunctions(app);
 // connectFunctionsEmulator(functions, "http://localhost", 5001);
-connectFunctionsEmulator(functions, "http://10.0.2.2", 5001);
+// connectFunctionsEmulator(functions, "http://127.0.0.1", 5001);
+// connectFunctionsEmulator(functions, "http://192.168.0.14", 5001);
+connectFunctionsEmulator(functions, "10.0.2.2", 5001);
 
 console.info("Initialised Functions");
 
 const auth = getAuth(app);
 // connectAuthEmulator(auth, "http://localhost:9099");
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
+// connectAuthEmulator(auth, "http://192.168.0.14:9099");
 connectAuthEmulator(auth, "http://10.0.2.2:9099");
 
 console.info("Initialised Auth");
